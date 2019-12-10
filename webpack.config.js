aac4f64f89dbc2cee134a48e27422f836f6c4560
wtong2017 <wtong@connect.ust.hk>
2019-12-10 12:06:27 +0800

    add more loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,25 @@
 var path = require('path');
 var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: ['webpack/hot/dev-server', './src/index.js'],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
