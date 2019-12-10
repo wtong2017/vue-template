b3291a339d94ccf14a8c0112902bd28261ce626a
wtong2017 <wtong@connect.ust.hk>
2019-12-10 16:08:13 +0800

    add sample component i.e. todo list

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
     <h1>Hello World!</h1>
+    <Todo></Todo>
   </div>
 </template>
+
+<script>
+import Todo from './components/Todo.vue'
+
+export default {
+    components: {
+        Todo,
+    }
+}
+</script>
---
+++ src/components/Todo.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div>Todo List</div>
+        <input type="text" v-model="todo">
+        <button @click="addItem">Add</button>
+        <ul v-for="(todo, i) in todos" :key="i">
+            <li>{{ todo }}<button @click="removeItem(i)">Delete</button></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            todo: '',
+            todos: ['hello', 'world']
+        }
+    },
+    methods: {
+        addItem() {
+            if (this.todo.length > 0) {
+                this.todos.push(this.todo)
+                this.todo = ''
+            }
+        },
+        removeItem(i) {
+            this.todos.splice(i, 1)
+        }
+    }
+}
+</script>
