1e591cc65af78aa60474c15172354a33b4e7f58f
wtong2017 <wtong@connect.ust.hk>
2020-07-22 17:28:21 +0800

    update vuejs version and test vuex

--- src/store.ts
+++ src/store.ts
@@ -2,10 +2,16 @@ import { createStore } from 'vuex'
 
 export type State = { count: number }
 
-export const store = createStore({
-  state() {
-    return {
-      count: 1
-    } as State
-  }
-})
+const state: State = { count: 0 }
+
+const mutations = {
+  increment(state: State) {
+    state.count++
+  },
+}
+
+const getters = {
+  count: () => state.count
+}
+
+export const store = createStore({ state, mutations, getters })
